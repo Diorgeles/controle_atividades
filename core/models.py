528690f9876f6526f8b0cef851ad31b9c4485531
aron <aronamaral@gmail.com>
2019-05-01 14:52:35 -0300

    corrigindo nome das variaveis

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ class Aluno(models.Model):
     nome = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20)
     nota = models.DecimalField(max_digits=3, decimal_places=2)
-    p_acertos = models.DecimalField(max_digits=3, decimal_places=2)
+    porcentagem_acertos = models.DecimalField(max_digits=3, decimal_places=2)
     aula = models.IntegerField()
 
     def __str__(self):
@@ -16,7 +16,7 @@ class Aluno(models.Model):
 
 class Categoria(models.Model):
 
-    cat = models.IntegerField()
+    categoria = models.IntegerField()
 
     def __str__(self):
         return str(self.cat)
