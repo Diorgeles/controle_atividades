5d2f4d1cc4c0c1dc783eb284e1aebc4001cfc185
aron <aronamaral@gmail.com>
2019-05-13 22:32:34 -0300

    insert_student

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, insert_student
 
 urlpatterns = [
-    path('', home)
+    path('', home, name='core_home'),
+    path('cadastrar-aluno', insert_student, name='core_insert_student'),
 ]
