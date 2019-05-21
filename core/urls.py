36c3ea347b28267497b8171e5f6e26de2927abfe
aron <aronamaral@gmail.com>
2019-05-20 21:40:55 -0300

    module list students

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, insert_student
+from .views import home, insert_student, list_student
 
 urlpatterns = [
     path('', home, name='core_home'),
     path('cadastrar-aluno', insert_student, name='core_insert_student'),
+    path('listar-aluno', list_student, name='core_list_student'),
 ]
