63117cc092dbeb7eebf81b703f26d6f4603621af
aron <aronamaral@gmail.com>
2019-05-20 23:23:46 -0300

    delete_student

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, insert_student, list_student, update_student
+from .views import home, insert_student, list_student, update_student, delete_student
 
 urlpatterns = [
     path('', home, name='core_home'),
     path('cadastrar-aluno', insert_student, name='core_insert_student'),
     path('listar-aluno', list_student, name='core_list_student'),
     path('atualizar-aluno/<int:id>', update_student, name='core_update_student'),
+    path('exluir-aluno/<int:id>', delete_student, name='core_delete_student'),
 ]
