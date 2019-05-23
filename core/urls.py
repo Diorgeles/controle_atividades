1994e79300a96f1d835eddb65035e9859925eaf9
aron <aronamaral@gmail.com>
2019-05-22 21:17:08 -0300

    insert course

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import home, insert_student, list_student, update_student, delete_student
+from .views import home,\
+    insert_student, \
+    list_student, \
+    update_student, \
+    delete_student, \
+    insert_activity
 
 urlpatterns = [
     path('', home, name='core_home'),
@@ -7,4 +12,6 @@ urlpatterns = [
     path('listar-aluno', list_student, name='core_list_student'),
     path('atualizar-aluno/<int:id>', update_student, name='core_update_student'),
     path('exluir-aluno/<int:id>', delete_student, name='core_delete_student'),
+    path('cadastrar-curso', insert_activity, name='core_insert_course'),
+
 ]
