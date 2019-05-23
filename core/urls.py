fd4e2d0a52f9c7d0ee40e84c709b6d5311d45bf4
aron <aronamaral@gmail.com>
2019-05-22 21:46:15 -0300

    correcting name url excluir

--- core/urls.py
+++ core/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('cadastrar-aluno', insert_student, name='core_insert_student'),
     path('listar-aluno', list_student, name='core_list_student'),
     path('atualizar-aluno/<int:id>', update_student, name='core_update_student'),
-    path('exluir-aluno/<int:id>', delete_student, name='core_delete_student'),
+    path('excluir-aluno/<int:id>', delete_student, name='core_delete_student'),
     path('cadastrar-curso', insert_activity, name='core_insert_course'),
 
 ]
